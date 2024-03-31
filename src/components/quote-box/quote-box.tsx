f0e2c7bb982500d8f68f63c574eaa2bbb9d57aeb
Thales Cardoso <canadathales@gmail.com>
2024-03-31 13:47:39 -0400

    Changed fetch strategy to use `react-query` in order to be able to cache results and handle errors with ease

--- src/components/quote-box/quote-box.tsx
+++ src/components/quote-box/quote-box.tsx
@@ -3,56 +3,47 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 import styles from "./quote-box.module.css";
 import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 const QuoteBox = () => {
-  const [quoteText, setQuoteText] = useState("");
-  const [quoteAuthor, setQuoteAuthor] = useState("");
-  const [quoteGenres, setQuoteGenres] = useState([]);
   const [activeGenre, setActiveGenre] = useState<string>("age");
-  const [isLoading, setIsLoading] = useState(true);
   const [backgroundImageUrl, setBackgroundImageUrl] = useState("");
 
-  useEffect(function fetchQuoteGenres() {
-    const apiUrl = "https://quote-garden.onrender.com/api/v3/genres";
+  const {
+    data: quotes,
+    isLoading,
+    isError,
+    refetch: refetchQuotes,
+  } = useQuery({
+    queryKey: ["quotes"],
+    queryFn: () =>
+      axios
+        .get(
+          `https://quote-garden.onrender.com/api/v3/quotes/random?genre=${activeGenre}`
+        )
+        .then((res) => res.data.data[0]),
+  });
 
-    axios
-      .get(apiUrl)
-      .then((response) => {
-        const data = response.data;
-        setQuoteGenres(data.data);
-      })
-      .catch((error) => {
-        console.error("There was an error fetching the quote:", error);
-      });
-  }, []);
+  const handleRefetch = () => {
+    refetchQuotes();
+  };
+
+  const { data: genres } = useQuery({
+    queryKey: ["genres"],
+    staleTime: Infinity,
+    queryFn: () =>
+      axios
+        .get(`https://quote-garden.onrender.com/api/v3/genres`)
+        .then((res) => res.data.data),
+  });
 
   useEffect(() => {
-    fetchNewQuote();
-  }, []);
+    fetchBackgroundImage();
+  }, [quotes]);
 
   const chooseGenre = (element: ChangeEvent<HTMLSelectElement>) => {
     const genre = element?.target?.value;
     setActiveGenre(genre);
-  };
-
-  const fetchNewQuote = async () => {
-    setIsLoading(true);
-    await fetchQuote();
-    await fetchBackgroundImage();
-    setIsLoading(false);
-  };
-
-  const fetchQuote = async () => {
-    try {
-      const response = await axios.get(
-        `https://quote-garden.onrender.com/api/v3/quotes/random?genre=${activeGenre}`
-      );
-      const data = response.data.data[0];
-      setQuoteText(data.quoteText);
-      setQuoteAuthor(data.quoteAuthor);
-    } catch (error) {
-      console.error("Error fetching quote:", error);
-    }
   };
 
   const fetchBackgroundImage = async () => {
@@ -68,9 +59,8 @@ const QuoteBox = () => {
   };
 
   const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
-    `"${quoteText}" - ${quoteAuthor}`
+    `"${quotes?.quoteText}" - ${quotes?.quoteAuthor}`
   )}`;
-
   return (
     <>
       <div
@@ -79,43 +69,56 @@ const QuoteBox = () => {
         style={{ backgroundImage: `url(${backgroundImageUrl})` }}
       ></div>
       <div id="quote-box" className={styles.quote_box}>
-        <div className={styles.quote_inner}>
-          {isLoading ? (
-            <div className={styles.spinner}></div>
-          ) : (
+        {isError ? (
+          <div className={styles.quote_inner}>
             <div id="quote-wrapper" className={styles.quote_wrapper}>
               <q id="text" className={styles.quote_text}>
-                {quoteText}
+                Error fetching quote 😢
               </q>
               <p id="author" className={styles.quote_author}>
-                {quoteAuthor}
+                Please try reloading the page
               </p>
             </div>
-          )}
-          <div id="quote-options" className={styles.quote_options}>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={tweetUrl}
-              id="tweet-quote"
-              className={styles.tweet_quote}
-            >
-              <FontAwesomeIcon icon={faTwitter} />
-            </a>
-            <select onChange={chooseGenre}>
-              {quoteGenres.map((genre, index) => (
-                <option key={`quotes_${index}`}>{genre}</option>
-              ))}
-            </select>
-            <button
-              onClick={fetchNewQuote}
-              id="new-quote"
-              className={styles.new_quote}
-            >
-              New Quote
-            </button>
           </div>
-        </div>
+        ) : (
+          <div className={styles.quote_inner}>
+            {isLoading ? (
+              <div className={styles.spinner}></div>
+            ) : (
+              <div id="quote-wrapper" className={styles.quote_wrapper}>
+                <q id="text" className={styles.quote_text}>
+                  {quotes?.quoteText}
+                </q>
+                <p id="author" className={styles.quote_author}>
+                  {quotes?.quoteAuthor}
+                </p>
+              </div>
+            )}
+            <div id="quote-options" className={styles.quote_options}>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={tweetUrl}
+                id="tweet-quote"
+                className={styles.tweet_quote}
+              >
+                <FontAwesomeIcon icon={faTwitter} />
+              </a>
+              <select onChange={chooseGenre}>
+                {genres?.map((genre: string, index: number) => (
+                  <option key={`quotes_${index}`}>{genre}</option>
+                ))}
+              </select>
+              <button
+                onClick={handleRefetch}
+                id="new-quote"
+                className={styles.new_quote}
+              >
+                New Quote
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
